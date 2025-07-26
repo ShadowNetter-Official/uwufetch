bfa6c40865f51f25d3ba2893ea360488ee03c9ce
ShadowNetter <shadownetter@gmail.com>
2025-07-26 22:40:02 +0200

    initial commit

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo
+echo "uwufetch"
+echo "by ShadowNetter"
+echo
+echo "cloning repo..."
+git clone https://github.com/shadownetter-official/uwufetch
+cd uwufetch
+echo "done"
+echo
+echo "installing..."
+chmod +x uwufetch
+sudo cp uwufetch /bin/
+echo "done"
+echo
+echo "to uninstall: sudo rm /bin/uwufetch
+echo"
